35b34557c1f4b26e451fccfeee9656cc067255fe
Patrick Fasano <patrick@patrickfasano.com>
2017-02-20 13:16:49 -0500

    script: begin refactoring mfdn.py into submodules

--- script/mfdn/__init__.py
+++ script/mfdn/__init__.py
@@ -1,4 +1,4 @@
-"""mfdn.py -- define scripting for h2mixer+MFDn runs
+"""mfdn -- define scripting for h2mixer+MFDn runs
 
     Expected dictionary keys for h2mixer + mfdn run:
 
@@ -111,6 +111,7 @@
   - 2/10/17 (pjf):
     + Fix "fci" -> "-fci" flag
     + Add ndiag parameter to task dictionary.
+  - 2/20/17 (pjf): Rename mfdn.py -> mfdn/__init__.py
 """
 
 import datetime
@@ -121,6 +122,8 @@ import shutil
 import enum
 
 import mcscript
+
+from .utils import *
 
 ################################################################
 # radial basis modes
@@ -200,68 +203,6 @@ class TruncationMode(enum.Enum):
 k_truncation_mode_ho = TruncationMode.kHO
 k_truncation_mode_triangular = TruncationMode.kTriangular
 
-################################################################
-# physical constants
-################################################################
-
-k_mN_csqr = 938.92  # (m_N c^2)~938.92 MeV
-k_hbar_c = 197.327  # (hbar c)~197.327 Mev fm
-
-
-################################################################
-# utility calculations
-################################################################
-
-def weight_max_string(truncation):
-    """ Convert (rank,cutoff) to "wp wn wpp wnn wpn" string.
-
-    Valid truncations:
-        ("ob",w1b)
-        ("tb",w2b)
-        (w1b,w2b)
-        (wp,wn,wpp,wnn,wpn) -- TODO
-
-    >>> weight_max_string(("ob",4))
-        "4 4 8 8 8"
-    >>> weight_max_string(("tb",4))
-        "4 4 4 4 4"
-    """
-
-    if (truncation[0] == "ob"):
-        (code, N) = truncation
-        cutoffs = (N,N,2*N,2*N,2*N)
-    elif (truncation[0] == "tb"):
-        (code, N) = truncation
-        cutoffs = (N,N,N,N,N)
-    elif (len(truncation)==2):
-        (w1,w2) = truncation
-        cutoffs = (w1,w1,w2,w2,w2)
-    elif (len(truncation)==5):
-        cutoffs = truncation
-
-    return "{0[0]} {0[1]} {0[2]} {0[3]} {0[4]}".format(cutoffs)
-
-def oscillator_length(hw):
-    """ Calculate oscillator length for given oscillator frequency.
-
-    b(hw) = (hbar c)/[(m_N c^2) (hbar omega)]^(1/2)
-
-    Arguments:
-        hw (numeric): hbar omega in MeV
-
-    Returns:
-        (float): b in fm
-    """
-
-    return k_hbar_c/math.sqrt(k_mN_csqr*hw)
-
-def natural_orbital_indicator(natural_orbital_iteration):
-    """Construct natural orbital indicator string."""
-    if (natural_orbital_iteration is None):
-        indicator = ""
-    else:
-        indicator = "-no{:1d}".format(natural_orbital_iteration)
-    return indicator
 
 ################################################################
 # configuration
@@ -489,14 +430,6 @@ def task_descriptor_7(task):
         )
 
     return descriptor
-
-################################################################
-# observable definitions
-################################################################
-
-angular_momentum_operator_list = [
-    "L", "Sp", "Sn", "S", "J"
-]
 
 
 ##################################################################
@@ -889,10 +822,10 @@ def generate_tbme(task):
 
 
     # target: Hamiltonian
-    coef_Uksqr = (A-1)/(2*A)*hw+1/(2*A)*a_cm*(hw/hw_cm)
-    coef_Vk1k2 = -1/A*hw+1/A*a_cm*(hw/hw_cm)
     coef_Ursqr = 1/(2*A)*a_cm*(hw_cm/hw)
     coef_Vr1r2 = 1/A*a_cm*(hw_cm/hw)
+    coef_Uksqr = (A-1)/(2*A)*hw+1/(2*A)*a_cm*(hw/hw_cm)
+    coef_Vk1k2 = -1/A*hw+1/A*a_cm*(hw/hw_cm)
     coef_identity = -3/2*a_cm
     lines.append("define-target work/tbme-H.bin")
     lines.append("  add-source Ursqr {:e}".format(coef_Ursqr))
