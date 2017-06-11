8a32574b5756be5562837a0963396c0592023a53
Patrick Fasano <patrick@patrickfasano.com>
2017-06-10 23:01:54 -0400

    script: Fix v14 reference state input J->2J

--- script/mfdn/mfdn_v14.py
+++ script/mfdn/mfdn_v14.py
@@ -103,7 +103,7 @@ def run_mfdn(task, postfix=""):
 
     # obdme: write reference state list
     for reference_state in task["obdme_reference_state_list"]:
-        lines.append("{:d} {:d} {:d}".format(2*reference_state[0], reference_state[1], reference_state[2]))
+        lines.append("{:d} {:d} {:d}".format(int(2*reference_state[0]), reference_state[1], reference_state[2]))
 
     # ensure terminal line
     lines.append("")
