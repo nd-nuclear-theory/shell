d9c2cd8e3b8af062baa37cb88aea56edb6436026
Patrick Fasano <patrick@patrickfasano.com>
2017-06-13 15:44:24 -0700

    config: Update intel configs

--- config/mfdn/config-intel.mk
+++ config/mfdn/config-intel.mk
@@ -1,14 +1,15 @@
-# generic intel+ompi base definitions
+# generic intel+impi base definitions
 #
 # need to augment with MKL, ACML, or LAPACK
 #
 # 11/20/16 (mac): Created.
+# 06/13/17 (pjf): Switch to Intel MPI (included with ifort).
 
 # fortran
-FC := mpif90
-FFLAGS := -qopenmp 
+FC := mpiifort
+FFLAGS := -qopenmp
 FFLAGS += -O3
 
 # C
-CC := mpicc
+CC := mpiicc
 CFLAGS := $(FFLAGS)
