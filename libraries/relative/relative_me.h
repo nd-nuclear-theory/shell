56a789ffd6417d9a5719087fbb19124f1dd01a84
Mark A. Caprio <mcaprio-nd-edu>
2017-07-01 18:11:22 -0400

    relative: Add ConstructQuadrupoleOperator

--- libraries/relative/relative_me.h
+++ libraries/relative/relative_me.h
@@ -6,21 +6,28 @@
 
   (The identity operator is provided in basis/lsjt_operator.h.)
 
+  See notes on "internal representation of an operator in JT
+  scheme" in lsjt_operator.h for the general principles of how the
+  operators are represented.
+
   Language: C++11
                                  
   Mark A. Caprio
   University of Notre Dame
 
-  7/12/16 (mac): Created (as construct_relative).
-  3/26/17 (mac): Add ConstructCoulombOperator.
-  3/28/17 (mac): Remove ConstructDiagonalConstantOperator.  Rename to relative_me.
-
+  + 7/12/16 (mac): Created (as construct_relative).
+  + 3/26/17 (mac): Add ConstructCoulombOperator.
+  + 3/28/17 (mac):
+    - Rename to relative_me.
+    - Remove ConstructDiagonalConstantOperator.
+  + 7/1/17 (mac): Add ConstructQuadrupoleOperator.
 ****************************************************************/
 
 #ifndef RELATIVE_ME_H_
 #define RELATIVE_ME_H_
 
 #include "basis/lsjt_operator.h"
+#include "basis/proton_neutron.h"
 
 namespace relative {
 
@@ -47,13 +54,34 @@ namespace relative {
   //   relative_space (input) : target space
   //   relative_component_sectors (output) : target sectors
   //   relative_component_matrices (output) : target matrices
-  //   kinematic_operator (input): which operator to construct
+  //   kinematic_operator (input): whether to construct coordinate
+  //     or momentum operator
+
+  void ConstructQuadrupoleOperator(
+      const basis::OperatorLabelsJT& operator_labels,
+      const basis::RelativeSpaceLSJT& relative_space,
+      std::array<basis::RelativeSectorsLSJT,3>& relative_component_sectors,
+      std::array<basis::MatrixVector,3>& relative_component_matrices,
+      relative::KinematicOperator kinematic_operator,
+      basis::TwoBodySpeciesPN operator_species
+    );
+  // Construct quadrupole operator in relative LSJT basis.
+  //
+  // Arguments:
+  //   operator_labels (input) : tensorial properties of operator
+  //   relative_space (input) : target space
+  //   relative_component_sectors (output) : target sectors
+  //   relative_component_matrices (output) : target matrices
+  //   kinematic_operator (input): whether to construct coordinate
+  //     or momentum operator (coopted from ConstructKinematicOperator)
+  //   operator_species (input): operator for protons, neutrons, or both
 
   void ConstructCoulombOperator(
       const basis::OperatorLabelsJT& operator_labels,
       const basis::RelativeSpaceLSJT& relative_space,
       std::array<basis::RelativeSectorsLSJT,3>& relative_component_sectors,
       std::array<basis::MatrixVector,3>& relative_component_matrices,
+      basis::TwoBodySpeciesPN operator_species,
       int num_steps
     );
   // Construct coulomb operator in relative LSJT basis.
@@ -70,6 +98,7 @@ namespace relative {
   //   relative_space (input): target space
   //   relative_component_sectors (output): target sectors
   //   relative_component_matrices (output): target matrices
+  //   operator_species (input): operator for protons, neutrons, or both
   //   num_steps (input): number of integration steps
 
   ////////////////////////////////////////////////////////////////
