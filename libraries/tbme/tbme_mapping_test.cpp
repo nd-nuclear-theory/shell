b6916bd2f470e25893b990046621cbfec094d798
Mark A. Caprio <mcaprio-nd-edu>
2017-08-28 13:51:13 -0400

    tbme: tbme_mapping_test debugging of permuted orbitals (WIP)

--- libraries/tbme/tbme_mapping_test.cpp
+++ libraries/tbme/tbme_mapping_test.cpp
@@ -8,6 +8,8 @@
 
 #include "tbme/tbme_mapping.h"
 
+#include <algorithm>
+
 #include "cppformat/format.h"
 
 void TestMappingEqual()
@@ -85,6 +87,56 @@ void TestMappingSuperset()
 
 }
 
+void TestMappingRenumber()
+{
+
+  std::cout << "TestMappingRenumber" << std::endl;
+
+  // source space -- square
+  const int source_Nmax = 2;
+  const basis::OrbitalSpacePN source_orbital_space(source_Nmax);
+  const basis::TwoBodySpaceJJJPN source_space(
+      source_orbital_space,basis::WeightMax(basis::Rank::kTwoBody,source_Nmax)
+    );
+  std::cout << source_orbital_space.DebugStr();
+  std::cout << basis::OrbitalDefinitionStr(source_orbital_space.OrbitalInfo()) << std::endl;
+  std::cout << source_space.DebugStr();
+
+  // target space -- permuted
+  //
+  // reverse all orbitals (but it's okay that proton and neutron
+  // orbitals are no longer in order in the orbital info lists, as
+  // they will be partitioned back into their appropriate subspaces)
+
+  const int target_Nmax = 2;
+  std::vector<basis::OrbitalPNInfo> source_orbital_info = source_orbital_space.OrbitalInfo();
+  std::vector<basis::OrbitalPNInfo> target_orbital_info;
+  target_orbital_info.resize(source_orbital_info.size());
+  std::reverse_copy(
+      std::begin(source_orbital_info),std::end(source_orbital_info),
+      std::begin(target_orbital_info)
+    );
+  const basis::OrbitalSpacePN target_orbital_space(target_orbital_info);
+  const basis::TwoBodySpaceJJJPN target_space(
+      target_orbital_space,basis::WeightMax(basis::Rank::kTwoBody,target_Nmax)
+    );
+  std::cout << target_orbital_space.DebugStr() << std::endl;
+  std::cout << basis::OrbitalDefinitionStr(target_orbital_space.OrbitalInfo()) << std::endl;
+  std::cout << target_space.DebugStr();
+
+  // const basis::OrbitalSpacePN target_orbital_space(target_Nmax);
+  // const basis::TwoBodySpaceJJJPN target_space(source_orbital_space,basis::WeightMax(basis::Rank::kTwoBody,target_Nmax));
+
+  // set up mapping
+  // const shell::TwoBodyMapping two_body_mapping(
+  //     source_orbital_space,source_space,
+  //     target_orbital_space,target_space
+  //   );
+  // 
+  // std::cout << two_body_mapping.DebugStr() << std::endl;
+
+}
+
 ////////////////////////////////////////////////////////////////
 // main
 ////////////////////////////////////////////////////////////////
@@ -95,6 +147,7 @@ int main(int argc, char **argv)
   TestMappingEqual();
   TestMappingSubset();
   TestMappingSuperset();
+  TestMappingRenumber();
 
   // termination
   return 0;
