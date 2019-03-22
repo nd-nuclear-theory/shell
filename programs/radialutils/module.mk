9d0b328ff2010c56f74b6213ea5a4b5595e8e6df
Patrick Fasano <patrick@patrickfasano.com>
2019-03-22 16:42:00 -0400

    radialutils: Add orbital radial function extraction utilities

--- programs/radialutils/module.mk
+++ programs/radialutils/module.mk
@@ -9,6 +9,7 @@ module_units_cpp-h :=
 # module_units_f :=
 module_programs_cpp := orbital-gen radial-gen
 module_programs_cpp += radial-xform natorb-gen radial-compose
+module_programs_cpp += radial-extract
 # module_programs_cpp += radial-scale radial-stat
 
 # module_programs_f :=
