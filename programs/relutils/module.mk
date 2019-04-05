f610e88036aa1a276940774200c24977ed8d8a6d
Anna Mccoy <amccoy@triumf.ca>
2019-04-04 18:40:20 -0700

    Add converter from P. Navratil relative format to relative format

--- programs/relutils/module.mk
+++ programs/relutils/module.mk
@@ -7,7 +7,7 @@ $(eval $(begin-module))
 module_units_h := 
 module_units_cpp-h :=
 # module_units_f := 
-module_programs_cpp := relative-gen jpv2rel jpvstat
+module_programs_cpp := relative-gen jpv2rel jpvstat pn2rel
 module_programs_cpp += moshinsky 
 
 # module_programs_f :=
