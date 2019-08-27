5ebfa85766a439802e01214a7367d811e141ac88
Patrick Fasano <patrick@patrickfasano.com>
2019-08-27 17:15:13 -0400

    obme: Add reverse definition for RadialOperatorType

--- libraries/obme/module.mk
+++ libraries/obme/module.mk
@@ -4,8 +4,8 @@ $(eval $(begin-module))
 # unit definitions
 ################################################################
 
-module_units_h := obme_operator
-module_units_cpp-h := obme_io radial obme
+module_units_h :=
+module_units_cpp-h := obme_io radial obme obme_operator
 # module_units_f :=
 module_programs_cpp_test := obme_io_test obme_test
 
