3c11d787412bf09cea693b0b5e9b8c66ef3bf784
Mark A. Caprio <mcaprio-nd-edu>
2023-02-27 08:58:58 -0800

    programs/h2utils: Remove h2filter from module.mk

--- programs/h2utils/module.mk
+++ programs/h2utils/module.mk
@@ -7,7 +7,7 @@ $(eval $(begin-module))
 module_units_h :=
 module_units_cpp-h :=
 # module_units_f :=
-module_programs_cpp := h2stat h2conv h2mixer h2filter
+module_programs_cpp := h2stat h2conv h2mixer
 # module_programs_cpp_test += make-radial-dummy make-radial-permutation
 
 # module_programs_f :=
