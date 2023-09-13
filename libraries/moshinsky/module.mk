4bd3d705d51f1ba87a45bda0087c7c5fb9737c2f
Patrick Fasano <patrick@patrickfasano.com>
2023-09-12 19:28:19 -0500

    moshinsky: Fix moshinsky_bracket_table
    * Rewrite `moshinsky_bracket_table` for new indexing.
    * Update `basis`.

--- libraries/moshinsky/module.mk
+++ libraries/moshinsky/module.mk
@@ -8,7 +8,7 @@ module_units_h :=
 module_units_cpp-h := moshinsky_bracket moshinsky_xform
 # module_units_f :=
 module_programs_cpp_test := moshinsky_xform_test
-## module_programs_cpp += moshinsky_bracket_table
+module_programs_cpp_test += moshinsky_bracket_table
 
 # module_programs_f :=
 # module_generated :=
