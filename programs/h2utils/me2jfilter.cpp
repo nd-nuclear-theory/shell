5ca46f3a97f9253ac88c5b805be8fc18079237e1
Zhou Zhou <zzhou6@nd.edu>
2024-10-29 13:41:47 -0700

    programs/h2utils/me2jfilter: Add isovector and isotensor filtering

--- programs/h2utils/me2jfilter.cpp
+++ programs/h2utils/me2jfilter.cpp
@@ -4,7 +4,7 @@
 #include "tbme/tbme_scheme_xform.h"
 
 void TFilter(
-    size_t mode, // 0 for isoscalar, 1 for non-isoscalar
+    size_t mode, // 0 for isoscalar, 1 for non-isoscalar, 2 for isovector, 3 for isotensor
     const std::array<basis::TwoBodySectorsJJJT,3>& two_body_jjjt_component_sectors,
     std::array<basis::OperatorBlocks<double>,3>& two_body_jjjt_component_matrices
   ) {
@@ -13,6 +13,15 @@ void TFilter(
       basis::SetOperatorToZero(two_body_jjjt_component_sectors[2],two_body_jjjt_component_matrices[2]);
     } else if (mode == 1) {
       basis::SetOperatorToZero(two_body_jjjt_component_sectors[0],two_body_jjjt_component_matrices[0]);
+    } else if (mode == 2) {
+      basis::SetOperatorToZero(two_body_jjjt_component_sectors[0],two_body_jjjt_component_matrices[0]);
+      basis::SetOperatorToZero(two_body_jjjt_component_sectors[2],two_body_jjjt_component_matrices[2]);
+    } else if (mode == 3) {
+      basis::SetOperatorToZero(two_body_jjjt_component_sectors[0],two_body_jjjt_component_matrices[0]);
+      basis::SetOperatorToZero(two_body_jjjt_component_sectors[1],two_body_jjjt_component_matrices[1]);
+    } else {
+      std::cerr << "Invalid mode" << std::endl;
+      std::exit(EXIT_FAILURE);
     }
   }
 
@@ -26,33 +35,43 @@ struct RunParameters
   // filenames
   std::string input_filename;
   std::string output_filename;
-  // mode
+  // mode (is or nis)
+  int mode;
   int Nmax;
 };
 
 void ProcessArguments(int argc, char **argv, RunParameters& run_parameters)
 {
   // usage message
-  if (argc-1 < 3)
+  if (argc-1 < 4)
     {
-      std::cout << "Syntax: me2jfilter Nmax input_filename output_filename" << std::endl;
+      std::cout << "Syntax: me2jfilter Nmax mode(0:is, 1:nis, 2:iv, 3:it) input_filename output_filename" << std::endl;
       std::exit(EXIT_SUCCESS);
     }
 
   // format
-  std::istringstream parameter_stream(argv[1]);
-  parameter_stream >> run_parameters.Nmax;
-  if (!parameter_stream)
+  std::istringstream parameter_stream1(argv[1]);
+  parameter_stream1 >> run_parameters.Nmax;
+  if (!parameter_stream1)
     {
       std::cerr << "Expecting numeric value for Nmax argument" << std::endl;
       std::exit(EXIT_FAILURE);
     }
 
+  // input mode
+  std::istringstream parameter_stream2(argv[2]);
+  parameter_stream2 >> run_parameters.mode;
+  if (!parameter_stream2)
+    {
+      std::cerr << "Expecting numeric value for mode argument" << std::endl;
+      std::exit(EXIT_FAILURE);
+    }
+
   // input filename
-  run_parameters.input_filename = argv[2];
+  run_parameters.input_filename = argv[3];
 
   // output filename
-  run_parameters.output_filename = argv[3];
+  run_parameters.output_filename = argv[4];
 }
 
 int main(int argc, char **argv)
@@ -69,6 +88,7 @@ int main(int argc, char **argv)
   // set up parameters
   // std::string input_filename = "chi2bSMSI2C_srg0400_eMax16_EMax16_hwHO014.me2j.bin";
   int Nmax = run_parameters.Nmax;
+  int mode = run_parameters.mode;
   int J0 = 0;
   int g0 = 0;
   int Tz0 = 0;
@@ -89,7 +109,7 @@ int main(int argc, char **argv)
   shell::TransformOperatorTwoBodyJJJTTzToTwoBodyJJJT(two_body_jjjttz_space,two_body_jjjttz_sectors,two_body_jjjttz_matrices,
     two_body_jjjt_space,two_body_jjjt_component_sectors,two_body_jjjt_component_matrices
     );
-  TFilter(0,two_body_jjjt_component_sectors,two_body_jjjt_component_matrices);
+  TFilter(mode,two_body_jjjt_component_sectors,two_body_jjjt_component_matrices);
   shell::TransformOperatorTwoBodyJJJTToTwoBodyJJJTTz(
     two_body_jjjt_space,two_body_jjjt_component_sectors,two_body_jjjt_component_matrices,
     two_body_jjjttz_space,two_body_jjjttz_sectors,two_body_jjjttz_matrices
