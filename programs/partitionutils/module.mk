67690ecb0eff45e804c8cf963cf1a6bc6abc0338
Shwetha Vittal <svittal@nd.edu>
2024-11-14 13:55:20 -0500

    merge with feature/partitionutils to use group_read.cpp

---
+++ programs/partitionutils/module.mk
@@ -0,0 +1,16 @@
+$(eval $(begin-module))
+
+################################################################
+# unit definitions
+################################################################
+
+module_units_h :=
+module_units_cpp-h :=
+# module_units_f :=
+module_programs_cpp := group_read
+module_programs_cpp_test :=
+
+# module_programs_f :=
+# module_generated :=
+
+$(eval $(end-module))
