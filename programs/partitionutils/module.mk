ee6a63dd597a8e84563005e791cd8c9211c762aa
Mark A. Caprio <mcaprio-nd-edu>
2025-07-23 15:44:47 -0400

    partitionutils: Remove group_read from module.mk

--- programs/partitionutils/module.mk
+++ programs/partitionutils/module.mk
@@ -8,7 +8,7 @@ module_units_h :=
 module_units_cpp-h :=
 # module_units_f :=
 module_programs_cpp := smwf-convert smwf-truncate
-module_programs_cpp_test := group_read
+module_programs_cpp_test := # group_read introduces parallel_hashmap dependency
 
 # module_programs_f :=
 # module_generated :=
