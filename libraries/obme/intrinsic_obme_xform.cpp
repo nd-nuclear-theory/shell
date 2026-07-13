2b71e85e6007e026c425a50985b1d96baa7bcd0d
Mark A. Caprio <mcaprio-nd-edu>
2026-07-13 13:39:22 -0400

    intrinsic_obme_xform: Update sketch of indexing scheme

--- libraries/obme/intrinsic_obme_xform.cpp
+++ libraries/obme/intrinsic_obme_xform.cpp
@@ -24,18 +24,24 @@ namespace shell
   ///////////////////////////////////////////////////////////////
   // Indexing for one-body intrinsic density transformation matrix
   //
-  // Indexing for M matrix defined in (31) of Navratil 2021 or its inverse,
-  // broken into blocks by Delta_N.  We adopt a symmetric labeling
+  // For the M matrix (or its inverse) is defined in (31) of Navratil 2021, we
+  // define an indexing which recognizes that the matrix has a block sparsity
+  // structure when written in terms of blocks by Delta_N.  We adopt a symmetric
+  // labeling scheme
   //
   //     M(n1, l1, j1, n2, l2, j2; n1', l1', j1', n2', l2', j2')
   //
-  // for the matrix elements.
+  // for the matrix elements.  Compare original
+  //
+  //     M(n, l, j, n', l', j'; n1, l1, j1, n2, l2, j2)
+  //
+  // in (31) of Navratil 2021.
   //
   // For purposes of the indexing scheme, within the nomenclature of the basis
   // package, the row indices are "bra" state indices, and the column indices
   // are "ket" state indices.  However, physically, we are defining indexing,
   // not for a basis of states, but rather a basis of multipole operators on the
-  // single-particle space.
+  // single-particle space (a.k.a. fundamental multipole operators).
   //
   // The overall "state" indexing is provided by (n1,l1,j1,n2,l2,j2), where:
   //
@@ -45,20 +51,22 @@ namespace shell
   //    where N=2*n+l is the oscillator principal quantum number.
   //
   //  - The two orbitals must combine to give specified multipolarity J0 and
-  //    parity grade g.
+  //    parity grade g0 for the one-body operator.
   //
-  //  - We must impose some truncation on the orbitals or, more generallly,
-  //    orbital pairs included.  For one-body operators defined by a set of
-  //    OBMEs truncated according to N<=Nmax, we must at least include N1<=Nmax
-  //    and N2<=Nmax.
+  //  - We must impose some truncation on the orbitals or, more generally,
+  //    orbital pairs included.  To cover all indices appearing for a one-body
+  //    operator defined on a set of orbital truncated according to N<=Nmax, we
+  //    must at least include N1<=Nmax and N2<=Nmax.
   //
   // Then, the natural subspace structure for present purposes arises since the
   // Moshinsky brackets in (13) of Navratil 2021 enforce that the M matrix is
-  // block diagonal in the oscillator shell shift Delta_N=N1-N2 imposed by the
-  // one-body operator.  If our one-body operator is defined on a given set of
-  // orbitals with some one-body Nmax, we need to include at least the maximal
-  // Delta_N=Nmax arising in the OBMEs for this operator, while additional
-  // Delta_N subspaces are superfluous.
+  // block diagonal in the oscillator "shell shift" Delta_N=N1-N2 characterizing
+  // the fundamental one-body operators.  As for what truncation we can impose
+  // on Delta_N, if the one-body operators on which we will be acting with M in
+  // (31) of Navratil are defined on a given set of orbitals with some one-body
+  // Nmax, we need to include at least the maximal Delta_N=Nmax arising in the
+  // OBMEs for this operator, while additional Delta_N subspaces are
+  // superfluous.
   //
   // Within each Delta_N subspace, we need to include orbitals at least through
   // N1<=Nmax and N2<=Nmax, as noted above.  However, we *might* want to include
@@ -67,22 +75,33 @@ namespace shell
   // brackets, a truncation based on N_tot=N1+N2 would be convenient, in which
   // case N_tot=2*Nmax would be the minimum sufficient truncation.
   //
+  // However, OBMEs also involve a species s (or, equivalently, tz) dependence.
+  // If we include such a dependence in the orbital indexing for M, then the
+  // full labeling scheme for M becomes
+  //
+  //     M(s1, n1, l1, j1, s2, n2, l2, j2; s1', n1', l1', j1', s2', n2', l2', j2')
+  //
+  // the overall "state" indexing becomes (s1,n1,l1,j1,s2,n2,l2,j2), and the
+  // orbitals (s1,n1,l1,j1) and (s2,n2,l2,j2) must also now combine to give
+  // specified Tz0 for the one-body operator.
+  //
   ///////////////////////////////////////////////////////////////
   //
   // ## Labeling ##
   //
-  // subspace labels: (Delta_N, J0, g)
-  //
-  //   * Delta_N (int): Difference in principal quantum number (N2-N1).
+  // The space is naturally defined by the (J0, g0) of the one-body operator of
+  // interest:
   //
   //   * J0 (int): Multipolarity.
   //
-  //   * g (int): Grade (=0,1) for the parity P.
+  //   * g0 (int): Grade (=0,1) for the parity P.
   //
-  //   These labels are redundant, as the parity may be deduced from Delta_N, as
-  //   g~Delta_N.  However, the space is naturally defined by the (J0, g) of the
-  //   one-body operator of interest, so we retain g as a label for
-  //   transparency.
+  // subspace labels: (Delta_N)
+  //
+  //   * Delta_N (int): Difference in principal quantum number (N2-N1).
+  //
+  //   Note that Delta_N is constrained by its relation g0~Delta_N to the
+  //   parity grade.
   //
   // state labels within subspace: (n1, l1, j1, n2, l2, j2)
   //
@@ -93,24 +112,29 @@ namespace shell
   //   * l2 (int): Orbital angular momentum.
   //   * j2 (HalfInt): Total angular momentum.
   //
-  //   These labels are redundant, as n2 may be deduced from the others for
-  //   given Delta_N.
+  //   These labels exhibit some redundancy, as n2 may be deduced from the
+  //   others for given Delta_N.
   //
   //   The oscillator quantum number is deduced from the
   //   n and l quantum numbers:
   //
   //   * N1 (int): Oscillator quanta (N1=2*n1+l1).
-  //   * N2 (int): oscillator quanta (N2=2*n2+l2)/
+  //   * N2 (int): Oscillator quanta (N2=2*n2+l2).
   //
   ///////////////////////////////////////////////////////////////
   //
   // ## Subspaces ##
   //
-  // Within a full space defined by fixed (J0, g), and subject to
+  // Within a full space defined by fixed (J0, g0), and subject to
   // single-particle truncation Nmax, subspaces are ordered by:
   //
-  //    * Increasing delta_N (delta_N=0,1,...,Nmax), either all even or all odd,
-  //      as given by constraint delta_N~g.
+  //    * Increasing Delta_N (Delta_N=0,1,...,Nmax=Nmax_op), either all even or all odd,
+  //      as given by constraint delta_N~g0.
+  //
+  // For purposes of truncating Delta_N, we may, more specifically, use the
+  // one-body truncaiton Nmax=Nmax_op applied to the orbitals on which the OBMEs
+  // and densities appearing in (31) are defined.  Compare Nmax_mat below, used
+  // for the intermediate step of matrix inversion.
   //
   ///////////////////////////////////////////////////////////////
   //
@@ -130,9 +154,19 @@ namespace shell
   //
   // Constraints:
   //
-  //   * Orbital triangularity: triangle(l1,1/2,j1), triangle(l2,1/2,j2),
+  //   * Orbital triangularity: triangle(l1,1/2,j1), triangle(l2,1/2,j2).
+  //
+  //   * Orbital parity: l1+l2~g0, already enforced by Delta_N.
   //
   //   * Multipole triangularity: triangle(j1,j2,J0).
+  //
+  // For purposes of truncating states, we may truncate by some combination of
+  // N_tot<=N_tot_max ("triangular truncation"), as motivated above for
+  // convenience in truncation of the required set of Moshinsky coefficients,
+  // and N<=Nmax=Nmax_mat for the orbitals ("square truncation").  The minimal
+  // truncation providing the matrix elements appearing (31) is given by
+  // Nmax_mat=Nmax_op and N_tot_max=2*Nmax_op.  However, we may increase these
+  // cutoffs for improved accuracy in matrix inversion.
   //
   ///////////////////////////////////////////////////////////////
   
